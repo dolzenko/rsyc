bad8a96c78b70fac2a8c06c7e9162deb5303e7b4
Ev Dolzhenko <evgeniy.dolzhenko@blacksquaremedia.com>
2014-03-26 17:38:12 +0400

    Make Rails.application.config.app available in config/environments/*.rb files

--- lib/rsyc/railtie.rb
+++ lib/rsyc/railtie.rb
@@ -3,8 +3,8 @@ require "rsyc"
 
 module Rsyc
   class Railtie < Rails::Railtie
-    initializer "rsyc.init" do
-      Rails.application.config.app = Rsyc.new Rails.root.join('config', 'app.yml'), Rails.env
+    config.before_configuration do
+      config.app = Rsyc.new Rails.root.join('config', 'app.yml'), Rails.env
     end
   end
 end
